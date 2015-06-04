dd67f8e5a12bdb3608f8c50bf514089263c92922
RishabhPatel <rshbh.p@cognicsys.com>
2015-06-04 11:17:31 +0530

    PayPal code Upload

---
+++ PayPal/Controllers/PayPalController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Configuration;
+using PayPal.Models;
+
+namespace PayPal.Controllers
+{
+    public class PayPalController : Controller
+    {
+        //
+        // GET: /PayPal/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult ValidateCommand(string product, string totalPrice)
+        {
+            bool useSandbox = Convert.ToBoolean(ConfigurationManager.AppSettings["IsSandbox"]);
+            var paypal = new PayPalModel(useSandbox);
+
+            paypal.item_name = product;
+            paypal.amount = totalPrice;
+            return View(paypal);
+        }
+
+        public ActionResult RedirectFromPaypal()
+        {
+            return View();
+        }
+
+        public ActionResult CancelFromPaypal()
+        {
+            return View();
+        }
+
+        public ActionResult NotifyFromPaypal()
+        {
+            return View();
+        }
+
+
+    }
+}
